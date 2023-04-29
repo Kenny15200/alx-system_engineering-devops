770e7f71dbcd116c3e25238ec9286a0221c0d992
Kenny15200 <opaleyekehinde70@gmail.com>
2023-04-29 09:01:41 +0100

    Repetition Token #3

--- 0x06-regular_expressions/4-repetition_token_3.rb
+++ 0x06-regular_expressions/4-repetition_token_3.rb
@@ -1,3 +1,3 @@
 #!/usr/bin/env ruby
 #A ruby script to display character's repetition
-puts ARGV[0].scan(/[htbn{1,4}]/).join                                              
+puts ARGV[0].scan(/[htbn{11}]/).join                                              
