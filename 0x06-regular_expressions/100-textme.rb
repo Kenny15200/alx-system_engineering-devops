31a3a65ab3a15f96f074f343a24b647c20246138
Kenny15200 <opaleyekehinde70@gmail.com>
2023-04-30 08:17:14 +0100

    TextMe

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,6 +1,3 @@
 #!/usr/bin/env ruby
 # A regular expression that match a CAPITAL LETTER
-
-filename = "text_messages.log.txt"
-file_contents = File.read(filename)
-puts ARGV[0].scan(/\[from:(?<sender>[^\]]+)\].*\[to:(?<receiver>[^\]]+)\].*\[flags:(?<flags>[^\]]+)\]/).join
+puts ARGV[0].scan(/\[from:(.*?)\] \[to:(.*?)\] \[flags:(.*?)\]/).join(",")
